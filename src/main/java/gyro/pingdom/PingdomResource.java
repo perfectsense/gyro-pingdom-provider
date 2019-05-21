f32f037741d17f43b67394c7ccd3a59a814ce708
Hyoo Lim <hyoolim@gmail.com>
2019-05-21 14:24:11 -0400

    Changes credentials methods to be better typed.

--- src/main/java/gyro/pingdom/PingdomResource.java
+++ src/main/java/gyro/pingdom/PingdomResource.java
@@ -5,7 +5,7 @@ import gyro.core.resource.Resource;
 public abstract class PingdomResource extends Resource {
 
     protected <T> T createClient(Class<T> serviceClass) {
-        return ((PingdomCredentials) credentials()).createClient(serviceClass);
+        return credentials(PingdomCredentials.class).createClient(serviceClass);
     }
 
 }
